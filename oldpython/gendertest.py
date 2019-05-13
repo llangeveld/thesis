167cd7bed298826291e7e6771b253bb52ccc5ed1
Lonneke Langeveld <l.langeveld@gmail.com>
2019-05-13 11:27:04 +0200

    Fix duplicate code in gendertest script

--- oldpython/gendertest.py
+++ oldpython/gendertest.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
 import csv
 
-with open("resources/names_annotated.csv") as f:
-    genderFile = [line for line in csv.reader(f, delimiter=",")]
-
-fCount = 0
-mCount = 0
-nCount = 0
-femaleNames = []
-maleNames = []
-for name, gender in genderFile:
-    if gender == "F":
-        fCount += 1
-        femaleNames.append(name)
-    elif gender == "M":
-        mCount += 1
-        maleNames.append(name)
-    elif gender == "N":
-        nCount += 1
-
-print("Female: {0} | Male: {1} | Neutral: {2}".format(fCount, mCount, nCount))
-
 
 def get_gender():
     with open("resources/names_annotated.csv") as f:
@@ -34,6 +14,7 @@ def get_gender():
             maleNames.append(name)
 
     return(femaleNames, maleNames)
+
 
 def main():
     femaleNames, maleNames = get_gender()
