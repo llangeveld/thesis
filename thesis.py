928a51654acfe36f35ebb624b73cb1a7e72c57b4
Lonneke Langeveld <l.langeveld@gmail.com>
2019-06-02 16:21:20 +0200

    Add thesis-script documentation

--- thesis.py
+++ thesis.py
@@ -16,7 +16,8 @@ from scipy.sparse import hstack, coo_matrix
 
 
 def import_data():
-    """Returns #MeToo training- and testing files, and the feminism file."""
+    """Imports the data from the training-, testing- and feminism-files.
+    :return: The files, in list-form (2D)."""
     with open("resources/train.csv") as thing:
         f1 = [line for line in csv.reader(thing, delimiter=",")]
     with open("resources/feminismStance.csv") as thing2:
@@ -27,7 +28,9 @@ def import_data():
 
 
 def get_data(data):
-    """Takes a list and returns the differents elements (tweets, genders, stances"""
+    """Takes a list and returns the differents elements (tweets, genders, stances)
+    :param data: Matrix-list, with columns tweetID, tweet, gender, and stance
+    :return: Seperate lists with tweets, genders, and stances"""
     tweets = []
     genders = []
     stances = []
@@ -40,23 +43,31 @@ def get_data(data):
 
 
 def pre_process(tweets):
+    """Pre-processes the tweets by removing URLS,
+    lower-casing the tweets and transforming the
+    words to lemmas.
+    :param tweets: list of strings
+    :return: a list of pre-processed strings"""
     nlp = spacy.load('en')
     strTweets = []
-
     for tweet in tweets:
-        print(tweet)
         noURLS = re.sub(r"http\S+", "", tweet)
         tweetText = nlp(noURLS.lower())
         lemmas = []
         for token in tweetText:
             lemmas.append(token.lemma_)
-        print(' '.join(lemmas), "\n")
         strTweets.append(' '.join(lemmas))
 
     return strTweets
 
 
 def transform_tg(tweets, genders, le):
+    """Transforms the genders and makes one
+    features of genders + tweets using hstack
+    :param tweets: A list of vectors
+    :param genders: A list of strings
+    :param le: the gender's LabelEncoder
+    :return: The combined gender + tweet-features"""
     genders = le.transform(genders)
     genders = np.reshape(genders, (-1,1))
     tweets = coo_matrix(tweets)
@@ -65,6 +76,19 @@ def transform_tg(tweets, genders, le):
 
 
 def data_run(trainTweets, trainStances, testTweets, testStances, option, trainGenders=None, testGenders=None):
+    """Prepares the data for actual running, and sends the data through to
+    the linear SVC.
+    :param trainTweets: A list of strings
+    :param trainStances: A list of strings (all items being eiter 'F', 'N' or 'A')
+    :param testTweets: see trainTweets
+    :param testStances: see trainStances
+    :param option: A string to see what test is being run
+    :param trainGenders: An optional list of strings (all items being either 'F' or 'M'),
+    only if option == 'AG'
+    :param testGenders: see trainGenders
+    :return: the F1-score for the run test"""
+
+    # Vectorizer
     count_word = TfidfVectorizer(ngram_range=(3,3))
     count_char = TfidfVectorizer(analyzer='char', ngram_range=(2,3))
     vectorizer = FeatureUnion([('word', count_word), ('char', count_char)])
@@ -72,12 +96,15 @@ def data_run(trainTweets, trainStances, testTweets, testStances, option, trainGe
     trainTweets = vectorizer.transform(trainTweets)
     testTweets = vectorizer.transform(testTweets)
 
+    # For Task A w/ gender
     if option == "AG":
         le2 = preprocessing.LabelEncoder()
         le2.fit(trainGenders)
+        # Stack the features together
         trainTweets = transform_tg(trainTweets, trainGenders, le2)
         testTweets = transform_tg(testTweets, testGenders, le2)
 
+    # Transform stances
     le = preprocessing.LabelEncoder()
     le.fit(trainStances)
     trainStances = le.transform(trainStances)
@@ -88,6 +115,12 @@ def data_run(trainTweets, trainStances, testTweets, testStances, option, trainGe
 
 
 def run_tests(X_train, X_test, y_train, y_test):
+    """Runs the linear SVC.
+    :param X_train: List of features to train on
+    :param X_test: List of features to test on
+    :param y_train: List of labels for training
+    :param y_test: List of labels for testing
+    :return: F1-score"""
     svm = LinearSVC()
     svm.fit(X_train, y_train)
     predictions = svm.predict(X_test)
